64066ae05617e945265345aab7d7ab730ae32dc0
epraschifka <a1793469@adelaide.edu.au>
2024-02-05 13:19:24 +1030

    fixed loading page

--- bonsoir-app/src/pages/LoadingPage.js
+++ bonsoir-app/src/pages/LoadingPage.js
@@ -1,13 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import LoadingPage from "./LoadingPage";
 import ErrorPage from './ErrorPage';
 import LeftNav from "../components/navigation/LeftNav";
 import Conversation from '../components/conversation/Conversation';
 import './styles/HomePage.css';
 import TopNav from '../components/navigation/TopNav';
 
-function ConversationPage(props)
+function LoadingPage(props)
 {
     return (
         <>
@@ -20,4 +19,4 @@ function ConversationPage(props)
     )
 }
 
-export default ConversationPage;
+export default LoadingPage;
