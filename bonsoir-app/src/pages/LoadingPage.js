85de2f0e99b4db44c1895cf0c3c1c875b7fed272
epraschifka <a1793469@adelaide.edu.au>
2024-02-06 12:38:12 +1030

    fixed sticky nav bug

--- bonsoir-app/src/pages/LoadingPage.js
+++ bonsoir-app/src/pages/LoadingPage.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import ErrorPage from './ErrorPage';
 import LeftNav from "../components/navigation/LeftNav";
-import './styles/HomePage.css';
 import TopNav from '../components/navigation/TopNav';
 import Call from '../components/conversation/Call';
 import Chatlog from '../components/conversation/Chatlog';
