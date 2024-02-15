59d98177cdce225ffa440d842af75dd075598ef0
epraschifka <a1793469@adelaide.edu.au>
2024-02-15 12:43:17 +1030

    updated error page

--- bonsoir-app/src/pages/ErrorPage.js
+++ bonsoir-app/src/pages/ErrorPage.js
@@ -1,7 +1,17 @@
+import TopNav from '../components/navigation/TopNav';
 function ErrorPage()
 {
     return (
-        <h1>Page not found</h1>
+        <div>
+            <TopNav/>
+            <section id='home'>
+                <div className='intro-wrapper'>
+                    <div className='intro-text'>
+                        <h1>We couldn't find that page :(</h1>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
 
